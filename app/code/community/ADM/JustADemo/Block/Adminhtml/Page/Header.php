59f9ad2ed7b17df1f81ac3897dc76e2582d377cd
Vincent Pietri <vincent.pietri@gmail.com>
2014-04-10 17:21:39 +0200

    Ajout d'un bandeau dans le BO

--- app/code/community/ADM/JustADemo/Block/Adminhtml/Page/Header.php
+++ app/code/community/ADM/JustADemo/Block/Adminhtml/Page/Header.php
@@ -6,4 +6,12 @@ class ADM_JustADemo_Block_Adminhtml_Page_Header extends Mage_Adminhtml_Block_Pag
         parent::__construct();
         $this->setTemplate('adm/justademo/page/header.phtml');
     }
+
+    public function getTitle()
+    {
+        $config = Mage::getSingleton('core/resource')->getConnection('core_read')->getConfig();
+        $dbname = $config['dbname'];
+
+        return $this->__('JustADemo: read dbname "%s"',$dbname);
+    }
 }
