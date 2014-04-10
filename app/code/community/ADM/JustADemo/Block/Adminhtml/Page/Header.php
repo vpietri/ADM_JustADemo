4e0db5291609c3be754d605d68d7d691f3d36b69
Vincent Pietri <vincent.pietri@gmail.com>
2014-04-10 14:22:58 +0200

    Ajout d'un bandeau ... à compléter

---
+++ app/code/community/ADM/JustADemo/Block/Adminhtml/Page/Header.php
@@ -0,0 +1,9 @@
+<?php
+class ADM_JustADemo_Block_Adminhtml_Page_Header extends Mage_Adminhtml_Block_Page_Header
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->setTemplate('adm/justademo/page/header.phtml');
+    }
+}
