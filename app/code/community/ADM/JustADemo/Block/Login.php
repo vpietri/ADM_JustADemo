c4d2013fda444365c34383a87f67908f97c4aed3
Vincent Pietri <vincent.pietri@gmail.com>
2014-08-06 10:13:49 +0200

    Sort order on user select

--- app/code/community/ADM/JustADemo/Block/Login.php
+++ app/code/community/ADM/JustADemo/Block/Login.php
@@ -3,7 +3,9 @@ class ADM_JustADemo_Block_Login extends Mage_Adminhtml_Block_Template
 {
     public function getUsersJson()
     {
-        $obj = Mage::getModel('customer/customer')->getCollection()->setOrder('email', 'ASC');
+        $obj = Mage::getModel('customer/customer')->getCollection()
+                    ->setOrder('entity_id', 'DESC')
+                    ->setOrder('email', 'ASC');
         $obj->getSelect()->limit(20);
         $userList=array();
         foreach($obj as $user) {
